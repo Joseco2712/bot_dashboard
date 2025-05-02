bf359ad3542b803f06dbd8a4ebedcdcbeaa6b4ff
Joseco2712 <josequito2627@gmail.com>
2025-05-02 06:03:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function guardarConfiguracion() {
+    const prefix = document.getElementById("prefix").value;
+    alert(`Se guardaría el prefijo: ${prefix}`);
+    // Aquí luego conectarías con un backend para guardar la configuración real
+  }
+  
